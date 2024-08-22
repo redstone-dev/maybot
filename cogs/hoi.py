566f587cdc76b6bf4a0d80edb0389f5477407c7e
redstone-dev <jgit.overbill024@silomails.com>
2024-08-22 09:12:50 -0400

    refactoring but its broken

---
+++ cogs/hoi.py
@@ -0,0 +1,52 @@
+import nextcord
+from nextcord.ext import commands
+from dotenv import load_dotenv
+from os import environ
+from time import sleep
+
+load_dotenv()
+
+class HallOfInfamy(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+    
+    async def send_to_hoi(self, og_msg: nextcord.Message, user: nextcord.User, /, spoiler: bool):
+        embed_img = None
+        if len(og_msg.attachments) > 0:
+            embed_img = og_msg.attachments[0]
+
+        embed = nextcord.Embed(
+            title="#" + og_msg.channel.name,
+            description=f"{'||' if spoiler else ''}{og_msg.content}{'||' if spoiler else ''}",
+        ) 
+        embed.set_author(name=og_msg.author.display_name, icon_url=og_msg.author.display_avatar.url)
+        embed.set_image(url=embed_img) if embed_img is not None else None
+
+        await self.bot.get_channel(int(environ["HOI_CHANNEL_ID"])).send(
+            content=f"-# [jump to original message](<{og_msg.jump_url}>) | inducted by {user.mention}", 
+            embed=embed)
+    
+    @nextcord.message_command(name="Send to Hall of Infamy")
+    async def hoi_context_menu(self, interaction: nextcord.Interaction, message: nextcord.Message):
+        await self.send_to_hoi(message, interaction.user, spoiler=False)
+        await interaction.response.send_message("sent :thumbs_up:", ephemeral=True)
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: nextcord.RawReactionActionEvent):
+        self.remove_unwanted_hoi_posts()
+
+    async def remove_unwanted_hoi_posts(self):
+        try:
+            infamy_channel: nextcord.TextChannel = self.bot.get_channel(environ["HOI_CHANNEL_ID"])
+            messages = [
+            msg async for msg in infamy_channel.history(limit=1024)
+                if msg.author.id == self.bot.application_id
+            ]
+            for message in messages:
+                r_emojis = [r.emoji for r in message.reactions]
+
+                if "❌" in r_emojis and message.reactions[r_emojis.index("❌")].count >= int(environ["HOI_REACTION_REMOVAL_THRESH"]):
+                    await message.delete()    
+                    sleep(0.15)
+        except Exception as e:
+            print(e)
---
+++ cogs/oocqc.py
@@ -0,0 +1,69 @@
+import nextcord
+from nextcord.ext import commands
+from dotenv import load_dotenv
+from os import environ
+from random import choice
+
+load_dotenv()
+
+
+class OOCQC(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+    
+    def setup(self):
+        pass
+
+    @nextcord.slash_command
+    async def oocqc(interaction: nextcord.Interaction):
+        pass
+
+    @oocqc.slash_command(description="gets a specific string from oocqc")
+    async def string(self, interaction: nextcord.Interaction, string: str):
+        """
+        searches for a specific string in oocqc
+        """
+        with open("oocqc.txt", "rt") as oocqc_file:
+            oocqc_strings = oocqc_file.read().split("\n")
+            try:
+                results = [s for s in oocqc_strings if string.lower() in s.lower()]
+                results_formatted = nextcord.Embed(
+                    title=f"{len(results)} result{'s' if len(oocqc_strings) != 1 else ''} found for \"{string}\"",
+                    description="\n".join(results),
+                )
+                OOC_EMBED_CHAR_LIMIT = int(environ["OOC_EMBED_CHAR_LIMIT"])
+                if len(results_formatted) > OOC_EMBED_CHAR_LIMIT:
+                    results_formatted = nextcord.Embed(
+                        title=f"{len(results)} result{'s' if len(oocqc_strings) != 1 else ''} found for \"{string}\"",
+                        description="\n".join(results)[:OOC_EMBED_CHAR_LIMIT],
+                    )
+                    await interaction.response.send_message(
+                        embed=results_formatted,
+                        content=f"only showing first {OOC_EMBED_CHAR_LIMIT} characters",
+                    )
+                else:
+                    await interaction.response.send_message(embed=results_formatted)
+            except Exception as e:
+                await interaction.response.send_message(
+                    f"error. L bozo (probably <@{self.bot.owner_id}>'s fault)",
+                )
+                print(e)
+
+    @oocqc.slash_command(description="gets a random string from oocqc")
+    async def random(self, interaction: nextcord.Interaction):
+        with open("oocqc.txt", "rt") as oocqc_file:
+            oocqc_strings = oocqc_file.read().split("\n")
+            await interaction.response.send_message(choice(oocqc_strings))
+
+    @oocqc.slash_command(description="get the quote in the oocqc file at a specified line")
+    async def line(self, interaction: nextcord.Interaction, line: int):
+        """
+        specific line from oocqc file
+        """
+        with open("oocqc.txt", "rt") as oocqc_file:
+            oocqc_strings = oocqc_file.read().split("\n")
+            try:
+                assert line < len(oocqc_strings)
+            except:
+                await interaction.response.send_message("wow that line is out of this world!1!! L bozo", ephemeral=True)
+            await interaction.response.send_autocomplete(oocqc_strings[line - 1])
