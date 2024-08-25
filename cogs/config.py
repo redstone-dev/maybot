b2b84b2958fab9718d818488dadf6ca44b93fd6e
redstone-dev <jgit.overbill024@silomails.com>
2024-08-25 17:46:00 -0400

    give actual names to the config commands

--- cogs/config.py
+++ cogs/config.py
@@ -6,14 +6,14 @@ class ConfigCommands(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
     
-    @nextcord.slash_command()
+    @nextcord.slash_command(name="guild-config")
     async def config(self, interaction: nextcord.Interaction):
         pass
 
-    @config.subcommand(description="set which channel ID to use for the hall of infamy")
+    @config.subcommand(name="hoi-channel", description="set which channel ID to use for the hall of infamy")
     async def _cfg_set_hoi(self, interaction: nextcord.Interaction, id: int):
         global_conf["guild:" + interaction.guild_id]["hoi"]["channel_id"] = id
 
-    @config.subcommand(description="set which channel ID to use for the starboard")
+    @config.subcommand(name="starboard-channel", description="set which channel ID to use for the starboard")
     async def _cfg_set_stb(self, interaction: nextcord.Interaction, id: int):
         global_conf["guild:" + interaction.guild_id]["starboard"]["channel_id"] = id
